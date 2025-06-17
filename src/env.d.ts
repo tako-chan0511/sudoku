def42bf9f390d4f7b5d5ed0e53bf5b2f32e5c4f7
Hara Keisuke <harakeisuke7@gmail.com>
2025-06-17 21:13:53 +0900

    環境１

---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/client" />
+
+// virtual モジュールを明示的に定義
+declare module 'virtual:pwa-register'
+declare module 'virtual:pwa-register/vue'
